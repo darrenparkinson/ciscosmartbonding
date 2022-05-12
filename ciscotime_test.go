853ce83a93042e272dd1e0565bffeae592dda495
Darren Parkinson <dparkinson@natilik.com>
2022-05-12 12:45:28 +0100

    test: add ciscotime test

---
+++ ciscotime_test.go
@@ -0,0 +1,36 @@
+package ciscosmartbonding
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+)
+
+type TestCiscoDateTime struct {
+	Time CiscoDateTime `json:"time"`
+}
+
+func TestCiscoTime(t *testing.T) {
+	var tests = []struct {
+		name  string
+		input string
+		want  time.Time
+	}{
+		{"cisco time", `{"time": "2019-05-02T18:20:43.000+0000"}`, time.Date(2019, time.May, 2, 18, 20, 43, 0, time.FixedZone("", 0))},
+		{"RFC3339Nano time", `{"time": "2016-08-08T21:35:14.000052975+02:00"}`, time.Date(2016, time.August, 8, 21, 35, 14, 52975, time.FixedZone("", 7200))},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var got TestCiscoDateTime
+			err := json.Unmarshal([]byte(tt.input), &got)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !got.Time.Time.Equal(tt.want) {
+				t.Fatalf("returned %+v, wanted %+v", got, tt.want)
+
+			}
+		})
+	}
+}
